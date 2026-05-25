ec77f55cffd0034f85ff6b4ff331f49b3570e597
gsignorele <gsignorele@gmail.com>
2026-05-25 13:44:52 -0300

    tarjeta mejorada

--- app.py
+++ app.py
@@ -1128,6 +1128,45 @@ def matches():
             "Gales": url_for('static', filename='flags/wales.png'),
         }
     )
+
+@app.route("/admin/delete_match_by_teams")
+def delete_match_by_teams():
+
+    if not is_admin():
+        return redirect("/")
+
+    conn = get_db()
+    c = conn.cursor()
+
+    c.execute("""
+        SELECT id
+        FROM matches
+        WHERE home='Turquía'
+          AND away='República Democrática del Congo'
+    """)
+
+    row = c.fetchone()
+
+    if row:
+
+        match_id = row[0]
+
+        c.execute(
+            "DELETE FROM predictions WHERE match_id=?",
+            (match_id,)
+        )
+
+        c.execute(
+            "DELETE FROM matches WHERE id=?",
+            (match_id,)
+        )
+
+        conn.commit()
+
+    conn.close()
+
+    return redirect("/admin/matches")
+
 # =========================
 # PREDICT
 # =========================
